33c4fa301ecc780cbb7f26fcd7c30ca9bfebd67f
WONHO22 <dnjsgh2122@gmail.com>
2023-07-31 19:35:19 +0900

    [FEAT] SideBar 컴포넌트 초안 구현, country 국기 imgs 데이터 추가

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,5 +1,45 @@
+import React from "react";
+import { styled } from "styled-components";
+import country from "../data/country";
+
+const Cointainer = styled.section`
+  display: flex;
+  flex-direction: column;
+  > .title {
+    font-family: "Bagel Fat One";
+    font-size: 2rem;
+  }
+
+  > .flags > img {
+    width: 100px;
+    height: 100px;
+    border-radius: 100%;
+  }
+`;
+
+const CardButton = styled.div``;
+
 function SideBar() {
-  return <div>사이드바</div>;
+  return (
+    <Cointainer>
+      <div className="title">
+        <h1>
+          COCO <br />
+          trip
+        </h1>
+      </div>
+      <div className="flags">
+        국기
+        <img src={country[1].url} alt="USA" />
+      </div>
+      <div className="menu">
+        <CardButton>환율</CardButton>
+        <CardButton>팁</CardButton>
+        <CardButton>단위</CardButton>
+        <CardButton>할인율</CardButton>
+      </div>
+    </Cointainer>
+  );
 }
 
 export default SideBar;
---
+++ src/data/country.js
@@ -0,0 +1,33 @@
+import 미국 from "./imgs/미국.gif";
+import 일본 from "./imgs/일본.gif";
+import 중국 from "./imgs/중국.gif";
+import 필리핀 from "./imgs/필리핀.gif";
+import 베트남 from "./imgs/베트남.gif";
+import 태국 from "./imgs/태국.gif";
+import 싱가포르 from "./imgs/싱가포르.gif";
+import 인도네시아 from "./imgs/인도네시아.gif";
+import 캐나다 from "./imgs/캐나다.gif";
+import 말레이시아 from "./imgs/말레이시아.gif";
+import 미얀마 from "./imgs/미얀마.gif";
+import 오스트리아 from "./imgs/오스트리아.gif";
+import 독일 from "./imgs/독일.gif";
+import 인도 from "./imgs/인도.gif";
+
+const country = [
+  { name: "미국", url: 미국 },
+  { name: "일본", url: 일본 },
+  { name: "중국", url: 중국 },
+  { name: "필리핀", url: 필리핀 },
+  { name: "베트남", url: 베트남 },
+  { name: "태국", url: 태국 },
+  { name: "싱가포르", url: 싱가포르 },
+  { name: "인도네시아", url: 인도네시아 },
+  { name: "캐나다", url: 캐나다 },
+  { name: "말레이시아", url: 말레이시아 },
+  { name: "미얀마", url: 미얀마 },
+  { name: "오스트리아", url: 오스트리아 },
+  { name: "독일", url: 독일 },
+  { name: "인도", url: 인도 },
+];
+
+export default country;
