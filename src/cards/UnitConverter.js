b4049a0fcfd84ed6dbf55510487402db1676fcf8
WONHO22 <dnjsgh2122@gmail.com>
2023-07-31 14:12:34 +0900

    [FIX] Converter 컴포넌트 랜더링 안되는 버그 수정

--- src/cards/UnitConverter.js
+++ src/cards/UnitConverter.js
@@ -43,7 +43,7 @@ const UnitConverter = () => {
   const handleSwap = () => {
     setStandardUnit(conversionUnit);
     setConversionUnit(standardUnit);
-    setValue("");
+    // setValue("");
   };
 
   return (
--- src/components/Converter.js
+++ src/components/Converter.js
@@ -1,5 +1,60 @@
-function Converter() {
-  return <div>단위변환기</div>;
-}
+import React from "react";
+import { Tabs, Select } from "antd";
+import useConvert from "../hooks/useConvert";
+
+// const { TabPane } = Tabs;
+const { Option } = Select;
+
+const Converter = ({
+  category,
+  standardUnit,
+  conversionUnit,
+  value,
+  handleStandardUnitChange,
+  handleValueChange,
+  handleConversionUnitChange,
+  handleSwap,
+}) => {
+  const units = {
+    길이: ["cm", "m", "km", "in", "ft", "yd", "mi"],
+    넓이: ["m2", "ft2", "yd2", "km2", "mi2"],
+    무게: ["mg", "g", "kg", "oz", "lb"],
+    부피: ["mL", "L", "gal", "bbl", "oz"],
+    온도: ["C", "F"],
+  };
+
+  const { conversionValue } = useConvert(
+    category,
+    standardUnit,
+    conversionUnit,
+    value
+  );
+
+  return (
+    <div>
+      <div>
+        <Select value={standardUnit} onChange={handleStandardUnitChange}>
+          {units[category].map((unit) => (
+            <Option key={unit} value={unit}>
+              {unit}
+            </Option>
+          ))}
+        </Select>
+        <input type="text" value={value} onChange={handleValueChange} />
+      </div>
+      <div>
+        <Select value={conversionUnit} onChange={handleConversionUnitChange}>
+          {units[category].map((unit) => (
+            <Option key={unit} value={unit}>
+              {unit}
+            </Option>
+          ))}
+        </Select>
+        <span>{conversionValue}</span>
+      </div>
+      <button onClick={handleSwap}>Swap</button>
+    </div>
+  );
+};
 
 export default Converter;
