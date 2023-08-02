a7d526f6ac7b711db5766f25e37a4f45ccff3f11
WONHO22 <dnjsgh2122@gmail.com>
2023-08-02 19:24:06 +0900

    [FEAT] country 데이터 통화 기호 추가

--- src/data/country.js
+++ src/data/country.js
@@ -14,20 +14,90 @@ import 독일 from "./imgs/독일.gif";
 import 인도 from "./imgs/인도.gif";
 
 const country = [
-  { name: "미국", url: 미국, tip: true, currency: "USD" },
-  { name: "일본", url: 일본, tip: false, currency: "JPY" },
-  { name: "중국", url: 중국, tip: false, currency: "CNY" },
-  { name: "필리핀", url: 필리핀, tip: true, currency: "PHP" },
-  { name: "베트남", url: 베트남, tip: true, currency: "VND" },
-  { name: "태국", url: 태국, tip: true, currency: "THB" },
-  { name: "싱가포르", url: 싱가포르, tip: true, currency: "SGD" },
-  { name: "인도네시아", url: 인도네시아, tip: false, currency: "IDR" },
-  { name: "캐나다", url: 캐나다, tip: true, currency: "CAD" },
-  { name: "말레이시아", url: 말레이시아, tip: true, currency: "MYR" },
-  { name: "미얀마", url: 미얀마, tip: true, currency: "MMK" },
-  { name: "오스트리아", url: 오스트리아, tip: true, currency: "AUD" },
-  { name: "독일", url: 독일, tip: true, currency: "EUR" },
-  { name: "인도", url: 인도, tip: false, currency: "INR" },
+  {
+    name: "미국",
+    url: 미국,
+    tip: true,
+    currency: { base: "usd", kr: "달러", sign: "＄" },
+  },
+  {
+    name: "일본",
+    url: 일본,
+    tip: false,
+    currency: { base: "jpy", kr: "엔", sign: "￥" },
+  },
+  {
+    name: "중국",
+    url: 중국,
+    tip: false,
+    currency: { base: "cny", kr: "위안", sign: "￥" },
+  },
+  {
+    name: "필리핀",
+    url: 필리핀,
+    tip: true,
+    currency: { base: "php", kr: "페소", sign: "₱" },
+  },
+  {
+    name: "베트남",
+    url: 베트남,
+    tip: true,
+    currency: { base: "vnd", kr: "동", sign: "₫" },
+  },
+  {
+    name: "태국",
+    url: 태국,
+    tip: true,
+    currency: { base: "thb", kr: "밧", sign: "฿" },
+  },
+  {
+    name: "싱가포르",
+    url: 싱가포르,
+    tip: true,
+    currency: { base: "sgd", kr: "달러", sign: "S$" },
+  },
+  {
+    name: "인도네시아",
+    url: 인도네시아,
+    tip: false,
+    currency: { base: "idr", kr: "루피아", sign: "Rp" },
+  },
+  {
+    name: "캐나다",
+    url: 캐나다,
+    tip: true,
+    currency: { base: "cad", kr: "달러", sign: "CA$" },
+  },
+  {
+    name: "말레이시아",
+    url: 말레이시아,
+    tip: true,
+    currency: { base: "myr", kr: "링깃", sign: "RM" },
+  },
+  {
+    name: "미얀마",
+    url: 미얀마,
+    tip: true,
+    currency: { base: "mmk", kr: "짯", sign: "KS" },
+  },
+  {
+    name: "오스트리아",
+    url: 오스트리아,
+    tip: true,
+    currency: { base: "aud", kr: "실링", sign: "S" },
+  },
+  {
+    name: "독일",
+    url: 독일,
+    tip: true,
+    currency: { base: "eur", kr: "유로", sign: "€" },
+  },
+  {
+    name: "인도",
+    url: 인도,
+    tip: false,
+    currency: { base: "inr", kr: "루피", sign: "₹" },
+  },
 ];
 
 export default country;
